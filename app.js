9ce3ad065d4f0ca74eb08c21908c33dab27e8ef3
Seb <samm1497@hotmail.com>
2022-08-19 16:47:50 -0600

    9.2

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
-const fs = require('fs')
-const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 const profileDataArgs = process.argv.slice(2);
 
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
+console.log(profileDataArgs);
 
-const [name, github] = profileDataArgs
+const [name, github] = profileDataArgs;
 
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+console.log(name, github);
 
-//   console.log('================');
+const pageHTML = generatePage(name, github);
 
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-
-
-fs.writeFile('index.html', generatePage(name, github), err => {
+fs.writeFile('./index.html', pageHTML, err => {
   if (err) throw err;
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
-
-module.exports = generatePage;
